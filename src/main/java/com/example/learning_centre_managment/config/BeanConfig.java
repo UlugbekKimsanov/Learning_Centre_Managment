430d045ae400eefbdec20809ea4e818b7ef7a845
Ulug'bek <kimsanov0717@gmail.com>
2024-01-20 12:06:29 +0500

    .

--- src/main/java/com/example/learning_centre_managment/config/BeanConfig.java
+++ src/main/java/com/example/learning_centre_managment/config/BeanConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BeanConfig {
-
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
