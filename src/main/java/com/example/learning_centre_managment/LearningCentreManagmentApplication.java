e4d7ed01580a8330130c69e05c00c2466ace9b34
UzCoder <kimsanov0717@gmail.com>
2024-01-20 11:45:44 +0500

    Project Created

---
+++ src/main/java/com/example/learning_centre_managment/LearningCentreManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning_centre_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCentreManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningCentreManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learning_centre_managment/LearningCentreManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning_centre_managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCentreManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
